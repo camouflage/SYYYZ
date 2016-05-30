b68d95e85014694fd3c96e1bfa85599a7f852156
yinmingwang <972576849@qq.com>
2016-05-30 17:44:04 +0800

    add user

---
+++ app/src/main/java/com/example/hansity/syyyz/Instance.java
@@ -0,0 +1,97 @@
+package com.example.hansity.syyyz;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Created by samsung- on 2016/5/30.
+ */
+public class Instance {
+    public void main() {
+        TicketDBHelper ticketDBHelper = new TicketDBHelper(this);
+        TheaterDBHelper theaterDBHelper = new TheaterDBHelper(this);
+        Theater th1 = new Theater("TianHe Theater", "TianHe Road No.623 TianYu Square 5th Floor");
+        Theater th2 = new Theater("PingAn Theater", "LiWan Area ShiPu Road No.125");
+        Theater th3 = new Theater("SFC ShangHai Theater", "HaiZhu Area JiangYan Road YanHui Square 4th Floor");
+        Theater th4 = new Theater("IDC Star&Dream Theater", "BaiYun Area ShangJing Road No.1 ShangChen Cultural Activity Center");
+        Theater th5 = new Theater("BaoLi Theater", "BaiYun Area XiCha Road No.99 QiLi Square 5th Floor");
+        Theater th6 = new Theater("CheBei International Theater", "ZhongShan Road No.87 SuNing life Square 3th Floor");
+        Movie mov1 = new Movie();
+        String mov1description = "We find Steve Rogers leading the newly formed team of Avengers in their continued efforts to " +
+                "safeguard humanity. But after another incident involving the Avengers results " +
+                "in collateral damage, political pressure mounts to install a " +
+                "system of accountability, headed by a governing body to oversee and direct the " +
+                "team. The new status quo fractures the Avengers, resulting in two camps - one " +
+                "led by Steve Rogers and his desire for the Avengers to remain free to defend humanity " +
+                "without government interference, and the other following Tony Stark's " +
+                "surprising decision to support government oversight and accountability.";
+        mov1.setMovieDescription(mov1description);
+        mov1.setMovieName("Captain America: Civil War");
+        mov1.setMovieDirector("Anthony Russo, Joe Russo");
+        mov1.setMovieType("action/adventure/science fiction");
+        Bitmap bitmapmov1 = BitmapFactory.decodeResource(this.getBaseContext().getResources(), R.drawable.mov1);
+        mov1.setMovieImage(bitmapmov1);
+        mov1.setMovieDuration(165);
+        Movie mov2 = new Movie();
+        String mov2description = "Since the dawn of civilization, he was worshipped as a god. " +
+                "Apocalypse, the first and most powerful mutant from Marvel's X-Men universe," +
+                " amassed the powers of many other mutants, becoming immortal and invincible. " +
+                "Upon awakening after thousands of years, he is disillusioned with the world as he " +
+                "finds it and recruits a team of powerful mutants, including a disheartened Magneto, " +
+                "to cleanse mankind and create a new world order, over which he will reign. As the fate of " +
+                "the Earth hangs in the balance, Raven with the help of Professor X must lead a team of young " +
+                "X-Men to stop their greatest nemesis and save mankind from complete destruction.";
+        mov2.setMovieDescription(mov2description);
+        mov2.setMovieDuration(145);
+        Bitmap bitmapmov2 = BitmapFactory.decodeResource(this.getBaseContext().getResources(), R.drawable.mov2);
+        mov2.setMovieImage(bitmapmov2);
+        mov2.setMovieType("Action/Adventure");
+        mov2.setMovieDirector("Bryan Singer");
+        mov2.setMovieName("X-MEN: APOCALYPSE");
+        Movie mov3 = new Movie();
+        String mov3description = "Courtship or conquest? The breakdown of a defiant spirit or a breakthrough that liberates " +
+                "a heart deprived of love? From the world-renowned Stratford Festival, this scintillating production " +
+                "of Shakespeare's boisterous comedy The Taming of the Shrew will stir your emotions as you follow " +
+                "the love story of Katherina and Petruchio - the feisty lovers at the center of Shakespeare's most " +
+                "pointed social commentary. Deborah Hay gives a “performance for the ages” as the Shrew, " +
+                "and Ben Carlson is the perfect counterpoint as her plotting suitor, creating a chemistry that " +
+                "sets the stage alight.";
+        mov3.setMovieDescription(mov3description);
+        mov3.setMovieName("STRATFORD FESTIVAL: THE TAMING OF THE SHREW");
+        mov3.setMovieDirector("Chris Abraham, Barry Avrich");
+        mov3.setMovieType("Program");
+        mov3.setMovieDuration(173);
+        Bitmap bitmapmov3 = BitmapFactory.decodeResource(this.getBaseContext().getResources(), R.drawable.mov3);
+        mov3.setMovieImage(bitmapmov3);
+        Movie mov4 = new Movie();
+        String mov4description = "Alice Kingsleigh (Wasikowska) has spent the past few years following in her father's " +
+                "footsteps and sailing the high seas. Upon her return to London, she comes across a magical looking " +
+                "glass and returns to the fantastical realm of Underland and her friends the White Rabbit (Sheen), " +
+                "Absolem (Rickman), the Cheshire Cat (Fry) and the Mad Hatter (Depp), who is not himself. The Hatter " +
+                "has lost his Muchness, so Mirana (Hathaway) sends Alice on a quest to borrow the Chronosphere, " +
+                "a metallic globe inside the chamber of the Grand Clock which powers all time. Returning to the past, " +
+                "she comes across friends - and enemies - at different points in their lives, " +
+                "and embarks on a perilous race to save the Hatter before time runs out.";
+        Bitmap bitmapmov4 = BitmapFactory.decodeResource(this.getBaseContext().getResources(), R.drawable.mov4);
+        mov4.setMovieImage(bitmapmov4);
+        mov4.setMovieDuration(113);
+        mov4.setMovieType("Action/Adventure");
+        mov4.setMovieDirector("James Bobin");
+        mov4.setMovieName("ALICE THROUGH THE LOOKING GLASS");
+        Movie mov5 = new Movie();
+        String mov5description = "Gaza. Synonymous to so many with conflict, destruction and despair but to Mohammed Assaf, " +
+                "and his sister Nour, Gaza is their home and their playground. Their band might play on second hand, " +
+                "beaten up instruments but their ambitions are sky-high. And so, in front of him on TV one evening lies " +
+                "an impossible dream: the auditions for Arab Idol, the most popular show in the Arab world, are taking place " +
+                "in Cairo. The borders are closed. There is no way out. Somehow, he finds a way and makes it in front of the " +
+                "judges in Egypt. From there, destiny awaits, a chance to change his life and give a voiceless people the " +
+                "greatest feeling of all: the freedom to love, live and feel free.";
+        mov5.setMovieDescription(mov5description);
+        mov5.setMovieName("THE IDOL (YA TAYR EL TAYER)");
+        mov5.setMovieDirector("Hany Abu-Assad");
+        mov5.setMovieType("Drama");
+        mov5.setMovieDuration(95);
+        Bitmap bitmapmov5 = BitmapFactory.decodeResource(this.getBaseContext().getResources(), R.drawable.mov5);
+        mov5.setMovieImage(bitmapmov5);
+    }
+}
